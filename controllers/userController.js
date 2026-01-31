b6d964d50929aa0e7b914182c000e253d95d8fef
nidheesh977 <bnidheesh844@gmail.com>
2026-01-31 16:58:14 +0530

    true and none

--- controllers/userController.js
+++ controllers/userController.js
@@ -81,7 +81,7 @@ const login = async (req, res) => {
   
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite: "none",
       maxAge: 60 * 60 * 1000
     });
